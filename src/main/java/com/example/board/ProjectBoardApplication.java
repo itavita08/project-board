facd0ec4b8b755e04c2d9bd4cb1298092a800f13
itavita08 <dudwns10000@gmail.com>
2023-04-23 22:22:13 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능하다
    
    `@MappedSuperclass` 이용하여 상속 방식으로 추출함
    
    그외 `@DateTimeFormat` 요소 추가 및 jpa 옵션 개선

--- src/main/java/com/example/board/ProjectBoardApplication.java
+++ src/main/java/com/example/board/ProjectBoardApplication.java
@@ -2,11 +2,7 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-//@ConfigurationPropertiesScan
-@EntityScan(basePackages = {"com.example.board"})
 @SpringBootApplication
 public class ProjectBoardApplication {
 
