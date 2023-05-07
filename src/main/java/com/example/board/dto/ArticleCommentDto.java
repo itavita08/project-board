c9f31c8191459bc63e61180b6527a7ecec454a6e
itavita08 <dudwns10000@gmail.com>
2023-05-07 17:31:56 +0900

    #18 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ src/main/java/com/example/board/dto/ArticleCommentDto.java
@@ -0,0 +1,10 @@
+package com.example.board.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(LocalDateTime createAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+
+    public static ArticleCommentDto of(LocalDateTime createAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
