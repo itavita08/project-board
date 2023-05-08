0d6f3a8dbdf767dfbfbda86454592a222478fdb9
itavita08 <dudwns10000@gmail.com>
2023-05-08 23:53:06 +0900

    #18 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/test/java/com/example/board/service/ArticleServiceTest.java
+++ src/test/java/com/example/board/service/ArticleServiceTest.java
@@ -56,14 +56,14 @@ class ArticleServiceTest {
         SearchType searchType = SearchType.TITLE;
         String searchKeyword = "title";
         Pageable pageable = Pageable.ofSize(20);
-        given(articleRepository.findByTitle(searchKeyword, pageable)).willReturn(Page.empty());
+        given(articleRepository.findByTitleContaining(searchKeyword, pageable)).willReturn(Page.empty());
 
         // When
         Page<ArticleDto> articles = sut.searchArticles(searchType, searchKeyword, pageable);
 
         // Then
         assertThat(articles).isEmpty();
-        then(articleRepository).should().findByTitle(searchKeyword, pageable);
+        then(articleRepository).should().findByTitleContaining(searchKeyword, pageable);
     }
 
     @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
@@ -155,7 +155,7 @@ class ArticleServiceTest {
     void givenArticleId_whenDeletingArticle_thenDeletesArticle(){
 
         Long articleId = 1L;
-        willDoNothing().given(articleRepository).delete(any(Article.class));
+        willDoNothing().given(articleRepository).deleteById(articleId);
 
         sut.deleteArticle(1L);
 
